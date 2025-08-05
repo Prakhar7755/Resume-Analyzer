57c6ab2d9d19c7bb1318d7df14988809660265fd
Prakhar_Github <prakhar.5779.aps3@gmail.com>
2025-08-05 15:46:30 +0530

    Build Navbar and Card component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants/index";
+import ResumeCard from "~/components/ResumeCard";
+// import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer - Prakhar" },
+    { name: "description", content: "Feedback for you resume" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Application & Resume Ratings</h1>
+          <h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid,
+            eveniet.
+          </h2>
+        </div>
+
+      {
+        /* !loadingResumes &&*/ resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )
+      }
+      </section>
+    </main>
+  );
 }
