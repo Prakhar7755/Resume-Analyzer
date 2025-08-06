f48b51899d63bd9dbd3c69e8081338464648a6d9
Prakhar_Github <prakhar.5779.aps3@gmail.com>
2025-08-06 10:00:00 +0530

    Setup Auth and Puter API

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants/index";
 import ResumeCard from "~/components/ResumeCard";
+import { useEffect } from "react";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
 // import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
@@ -12,27 +15,31 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Application & Resume Ratings</h1>
-          <h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid,
-            eveniet.
-          </h2>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
         </div>
 
-      {
-        /* !loadingResumes &&*/ resumes.length > 0 && (
-          <div className="resumes-section">
-            {resumes.map((resume) => (
-              <ResumeCard key={resume.id} resume={resume} />
-            ))}
-          </div>
-        )
-      }
+        {
+          /* !loadingResumes &&*/ resumes.length > 0 && (
+            <div className="resumes-section">
+              {resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+              ))}
+            </div>
+          )
+        }
       </section>
     </main>
   );
